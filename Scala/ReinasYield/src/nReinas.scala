ef0a3d25b2b77163eb0be4106e72d55ed953660b
Bryan Jiménez Chacón <bsjc96@gmail.com>
2016-09-20 01:29:40 -0600

    Reinas Yield Scala

---
+++ Scala/ReinasYield/src/nReinas.scala
@@ -0,0 +1,37 @@
+object nReinas {
+  def main(args: Array[String]) = show(nreinas(4))
+
+  def nreinas(n: Int): Set[List[Int]] = {
+    def lugar(k: Int): Set[List[Int]] =
+      if (k == 0) Set(List())
+      else
+        for {
+          reinas <- lugar(k - 1)
+          col <- 0 until n
+          if posSegura(col, reinas)
+        } yield reinas :+ col
+
+    def posSegura(col: Int, reinas: List[Int]): Boolean = {
+      val fila = reinas.length
+      val reinasConFila = (0 until fila) zip reinas
+      reinasConFila forall {
+        case (r, c) => col != c && math.abs(col - c) != fila - r
+      }
+    }
+    lugar(n)
+  }
+  def show(solutions: Set[List[Int]]): Unit = {
+
+for (solution <- solutions) yield {
+      println("\n+" + "-+" * solution.length)
+      (for { col <- solution } yield {
+        println(
+        Vector.fill(solution.length + 1)("| ").updated(col, "|X").mkString("")
+        )
+        println("+" + "-+" * solution.length)
+      }).mkString("| \n+" + "-+" * solution.length + "\n")
+    }
+
+     
+  }
+}
